b4974383dde46e15dabe86d7a9969641e9d13141
Sahithyan <sahithyan2701@gmail.com>
2020-05-18 14:23:10 +0530

    hosting on heroku

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var data = require('./data.json');
 var app = express();
-var PORT_NUMBER = 80;
+var PORT_NUMBER = 3000;
+app.use(express.urlencoded({ extended: false }));
 app.get('/', function (req, res) {
     res.send("It's working");
 });
-app.get('/series/:id', function (req, res) {
-    var seriesId = req.params.id;
+app.get('/series', function (req, res) {
+    var seriesId = req.query.id;
+    if (seriesId == undefined) {
+        res.json({
+            message: "provide series parameter"
+        });
+    }
     if (Object.keys(data).includes(seriesId)) {
         res.json(data[seriesId]);
     }
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,21 @@ import express = require('express');
 const data = require('./data.json');
     
 const app = express();
-const PORT_NUMBER = 80;
+const PORT_NUMBER = 3000;
+
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', function (req, res) {
     res.send("It's working");
 })
 
-app.get('/series/:id', function (req, res) {
-    let seriesId = req.params.id;
+app.get('/series', function (req, res) {
+    let seriesId: string = <string>req.query.id;
+    if (seriesId == undefined) {
+        res.json({
+            message: "provide series parameter"
+        })
+    }
     if (Object.keys(data).includes(seriesId)) {
         res.json(data[seriesId]);
     } else {
